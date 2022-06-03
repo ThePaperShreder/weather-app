9591fd2821bcb5a5b221a5e5a4521a490317566f
Aleks <Loxakul@gmail.com>
2022-06-03 13:32:30 +0300

    first commit

---
+++ Test/my-app/src/App.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavComponent from './Header/NavComponent';
+import { Container, Row, Col} from 'react-bootstrap'
+
+function App() {
+  return (
+    <Container>
+  <Row>
+    <Col>
+      <NavComponent/>
+    </Col>
+  </Row>
+</Container>
+    
+  );
+}
+
+export default App;
